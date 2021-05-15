8c25f32a5eee56016d63adeef7108c873151c401
Konrad Pilewski <pilewski.konrad1@gmail.com>
2021-05-15 19:50:26 +0200

    Validation for duplicates

--- src/com/xemo/service/KanyeService.java
+++ src/com/xemo/service/KanyeService.java
@@ -7,13 +7,16 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.Set;
 
 public class KanyeService {
 
-    private Set<String> kanyeQuotes;
+    private Set<String> kanyeQuotes = new HashSet<>(19);
     private int userNumber = 0;
 
     public KanyeService(int userNumber) {
@@ -23,20 +26,19 @@ public class KanyeService {
     public String getKanyeQuote(){
         StringBuilder json = new StringBuilder();
         String formattedJSON = "";
-        for(int i = 0; i < userNumber; i++) {
+        while(kanyeQuotes.size() <= userNumber) {
             try {
-
-                URL url = new URL("https://api.kanye.rest/");
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+                URI url = new URI("https://api.kanye.rest/");
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.toURL().openStream(), StandardCharsets.UTF_8));
                 String line;
                 while ((line = in.readLine()) != null) {
                     json.append(line);
 
                     JSONObject jsonObject = new JSONObject(json);
-//                    formattedJSON += "Quote " + i + ": " + jsonObject.getJSONObject("quote") + "\n";
+                    formattedJSON += jsonObject.get("quote");
+                    kanyeQuotes.add(formattedJSON);
                 }
-            } catch (IOException e) {
+            } catch (IOException | JSONException | URISyntaxException e) {
                 e.printStackTrace();
             }
         }
